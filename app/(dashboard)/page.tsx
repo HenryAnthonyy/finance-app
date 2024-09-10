8113b029cc34d69909795205f164eea0727bc8f3
Henry Anthony <henrymeiv363@gmail.com>
2024-09-10 13:08:08 +0300

    chore: setting up UI and hooks for accounts post API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 export default function Home() {
-  return <div>home page</div>;
+  const { onOpen, onClose } = useNewAccount();
+
+  return (
+    <div>
+      home page
+      <Button onClick={onOpen}>open account</Button>
+    </div>
+  );
 }
--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,11 +1,13 @@
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
+import { createId } from "@paralleldrive/cuid2";
 
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
   const authenticated = getAuth(c);
 
   if (!authenticated?.userId) {
@@ -28,6 +30,29 @@ const app = new Hono().get("/", clerkMiddleware(), async (c) => {
     .where(eq(accounts.userId, authenticated.userId)); //accounts to be returned from only current user
 
   return c.json({ data });
-});
+})
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({
+      name: true,
+    })),
+    async(c) => {
+      const authenticated = getAuth(c)
+      const values = c.req.valid("json")
+
+      if(!authenticated?.userId) {
+        return c.json({error: "Unauthorized"}, 401)
+      }
+
+      const [data] = await db.insert(accounts).values({
+        id: createId(),
+        userId: authenticated?.userId,
+        ...values
+      }).returning(); // concat returning to be able to return values from db.insert
+
+      return c.json({data})
+    }
+  )
 
 export default app;
